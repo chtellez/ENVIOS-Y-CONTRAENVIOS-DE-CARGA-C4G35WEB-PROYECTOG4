488985cdf50a12fb1ae6caadd328db2ac1b211c0
DiDi7T <dtnirvana@gmail.com>
2021-11-07 23:08:25 -0500

    HU aceptaroferta terminado

--- front/src/components/Perfil.js
+++ front/src/components/Perfil.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Row, Col, Card, Accordion, Button } from "react-bootstrap";
+
 
 export default class Perfil extends Component {
-    render() {
-        return (
-            <div>
-                Holis
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </div>
+    );
+  }
 }

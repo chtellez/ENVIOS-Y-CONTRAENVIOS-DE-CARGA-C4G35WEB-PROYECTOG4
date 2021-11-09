d58de0e81a38358e7ff8010b880e5aba374c5d72
DiDi7T <dtnirvana@gmail.com>
2021-11-08 19:26:18 -0500

    actualizando rama

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "../pages/Home";
 import CrearOferta from "../pages/CrearOferta";
+import AceptarOferta from "../components/AceptarOferta";
+import Perfil from "../components/Perfil";
 
 // import CrearOferta from "../pages/CrearOferta";
 
@@ -16,6 +18,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/crear_oferta" element={<CrearOferta />}></Route>
+          <Route exact path="/AceptarOferta" element={<AceptarOferta />}></Route>
+          <Route exact path="/Perfil" element={<Perfil />}></Route>
         </Routes>
       </BrowserRouter>
     </div>

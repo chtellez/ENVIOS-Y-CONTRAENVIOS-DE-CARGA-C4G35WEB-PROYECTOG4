a3a4751684b1b5119e9d06b9cbba9cf5c0dfd78e
julian grajales <juliangrajales031119@gmail.com>
2021-11-13 11:47:31 -0500

    puliendo el css del login

--- front/src/pages/login.jsx
+++ front/src/pages/login.jsx
@@ -7,46 +7,61 @@ export default class login extends Component {
     return (
       <div id="container" class="col-12">
         <Card id="fondo" class="col-12">
-        
-        <Container id="login">
           
-          <Row>
-            <Col>
-            <Row>
-              <h2>Iniciar Sesión</h2>
-            </Row>
-            <Row>
-              <Col
-              sm= "12"
-              xs= "12"
-              md={{span:4,offset:4}}
-              lg={{ span:4, offset:4}}
-              xl= {{span:4, offset:4}}
-              >
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label> </Form.Label>
-                <Form.Control type="email" placeholder="Ingrese su email" />
-                
-              </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label><i class="fas fa-sign-out-alt"></i></Form.Label>
-                <Form.Control type="password" placeholder="Ingrese su contraseña" />
-              </Form.Group>
 
-              <Button variant="primary"> 
-                Ingresar
-              </Button>
-            </Form>
-            </Col>
+          <Container id="login">
+            <Row>
+              <Col>
+                <Row>
+                  <div id="fondo1">
+                  <div id="inicio">
+                    <h2>Iniciar Sesión</h2>
+                  </div>
+                  </div>
+                  
+                </Row>
+                <Row>
+                  <Col
+                    sm="12"
+                    xs="12"
+                    md={{ span: 4, offset: 4 }}
+                    lg={{ span: 4, offset: 4 }}
+                    xl={{ span: 4, offset: 4 }}
+                  >
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label> </Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Ingrese su email"
+                        />
+                      </Form.Group>
+
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label>
+                          <i class="fas fa-sign-out-alt"></i>
+                        </Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Ingrese su contraseña"
+                        />
+                      </Form.Group>
+
+                      <Button id="boton" variant="primary">
+                        Ingresar
+                      </Button>
+                    </Form>
+                  </Col>
+                </Row>
+              </Col>
             </Row>
-            </Col>
-            </Row>
-            
-        </Container>
+          </Container>
+          
         </Card>
-        
       </div>
     );
   }

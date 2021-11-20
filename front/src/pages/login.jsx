71ea2ca8e9185e689b3fa6585c8e69f808b61ac3
julian grajales <juliangrajales031119@gmail.com>
2021-11-20 18:33:50 -0500

    terminando el login

--- front/src/pages/login.jsx
+++ front/src/pages/login.jsx
@@ -8,27 +8,33 @@ export class Login extends React.Component {
 
   render() {
     return (
+      
       <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Login</div>
+        <br />
+        <div className="header">Inicio Sesión</div>
         <div className="content">
           <div className="image">
-            <img src={loginImg} />
+            <img src={loginImg} alt="fondo_login" />
           </div>
           <div className="form">
             <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="Ingrese su email" />
             </div>
             <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" name="password" placeholder="Ingrese su contraseña" />
             </div>
           </div>
         </div>
-        <div className="footer">
+        <div>
+        
+        <p>¿No tienes cuenta? <a href="/">Registrate!</a></p>
           <button type="button" className="btn">
             Login
           </button>
+          
+          <br /><br /><br />
         </div>
       </div>
     );

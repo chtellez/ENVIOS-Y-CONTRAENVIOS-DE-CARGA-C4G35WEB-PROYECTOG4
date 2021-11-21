115948344408a996542298ba1c1170404a91842c
DiDi7T <dtnirvana@gmail.com>
2021-11-20 20:30:20 -0500

    detalles pequeños

--- front/src/pages/AceptarOferta.jsx
+++ front/src/pages/AceptarOferta.jsx
@@ -1,180 +1,137 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
 import "../styles/Aoferta.css";
-import { Row, Col, Card, Accordion, Button, Image } from "react-bootstrap";
+import { Row, Col, Card, Accordion, Button, Modal,} from "react-bootstrap";
 import img from "../assets/Hand-Truck.svg";
 
-export default class AceptarOferta extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="contenedor">
-          <section className="section">
-            <Row>
-              <Col
-                xs={6}
-                sm={6}
-                md={{ span: 4, offset: 1 }}
-                lg={{ span: 4, offset: 1 }}
-              >
-                <div className="content">
-                  <h2>MIS OFERTAS </h2>
-                  <p>
-                    Visualiza las ofertas que has publicado y las respuestas de
-                    transportistas.
-                  </p>
-                </div>
-              </Col>
-              <Col
-                xs={6}
-                sm={6}
-                md={{ span: 4, offset: 2 }}
-                lg={{ span: 4, offset: 2 }}
-                className="align-self-end"
-              >
-                <img src={img} className="img" />
-              </Col>
-              <Col>
-                <svg
-                  className="wave"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 1440 320"
-                >
-                  <path
-                    fill="#fff"
-                    // fill-opacity="1"
-                    d="M0,160L48,170.7C96,181,192,203,288,186.7C384,171,480,117,576,112C672,107,768,149,864,165.3C960,181,1056,171,1152,149.3C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-                  ></path>
-                </svg>
-              </Col>
-            </Row>
-          </section>
-          <br></br>
-          <Row className="ml-5 justify-content-center" style={{ marginLeft: "22px" }}>
-            <Col xs={6} sm={6} md={3} lg={3} className="mb-5">
-              <Card style={{ width: "auto" }} className="Cardsita">
-                <Card.Header className="cabecera">Oferta 1</Card.Header>
-                <Card.Body>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    Descripcion breve de la oferta
-                  </Card.Subtitle>
-                  <Card.Text>
-                    <Accordion defaultActiveKey="1" flush>
-                      <Accordion.Item eventKey="0">
-                        <Accordion.Header>Ver propuestas</Accordion.Header>
-                        <Accordion.Body>
-                          <Row className="ml-5 justify-content-center">
-                            <Col xs={12} sm={12} md={12} lg={12} className="mb-5">
-                              <Card style={{ width: "auto" }}>
-                                <Card.Header>Propuesta 1</Card.Header>
-                                <Card.Body>
-                                  <Card.Subtitle className="mb-2 text-muted">
-                                    Contraoferta de transportista
-                                  </Card.Subtitle>
-                                  <Card.Text>uwu</Card.Text>
-                                  <Button
-                                    className="justify-content-end"
-                                    href="#"
-                                  >
-                                    Pagar
-                                  </Button>
-                                </Card.Body>
-                              </Card>
-                            </Col>
-                          </Row>
+function AceptarOferta() {
+  const [show, setShow] = useState(false);
 
-                          <br></br>
-                          <Card style={{ width: "auto" }}>
-                            <Card.Header>Propuesta 1</Card.Header>
-                            <Card.Body>
-                              <Card.Subtitle className="mb-2 text-muted">
-                                Contraoferta de transportista
-                              </Card.Subtitle>
-                              <Card.Text>uwu</Card.Text>
-                              <Button
-                                variant="warning"
-                                className="pagar"
-                                href="#"
-                              >
-                                Pagar
-                              </Button>
-                            </Card.Body>
-                          </Card>
-                        </Accordion.Body>
-                      </Accordion.Item>
-                    </Accordion>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+    
+  return (
+    <React.Fragment>
+      <div className="contenedor">
+        <section className="section">
+          <Row>
+            <Col
+              xs={6}
+              sm={6}
+              md={{ span: 4, offset: 1 }}
+              lg={{ span: 4, offset: 1 }}
+            >
+              <span className="content">
+                <h2>MIS OFERTAS </h2>
+                <p>
+                  Visualiza las ofertas que has publicado y las respuestas
+                  de transportistas.
+                </p>
+              </span>
             </Col>
-
-
-            <Col xs={6} sm={6} md={3} lg={3} className="mb-5">
-              <Card style={{ width: "auto", borderRadius: "20px", elevation: 3 }} className="Cardsita">
-                <Card.Header style={{ backgroundColor:"#53b7f1",  borderTopLeftRadius: 20, 
-                borderTopRightRadius: 20, }}>Oferta 1</Card.Header>
-                <Card.Body>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    Descripcion breve de la oferta
-                  </Card.Subtitle>
-                  <Card.Text>
-                    <Accordion defaultActiveKey="1" flush>
-                      <Accordion.Item eventKey="0">
-                        <Accordion.Header>Ver propuestas</Accordion.Header>
-                        <Accordion.Body>
-                          <Row className="ml-5 justify-content-center">
-                            <Col xs={12} sm={12} md={12} lg={12} className="mb-5">
-                              <Card style={{ width: "auto" }}>
-                                <Card.Header>Propuesta 1</Card.Header>
-                                <Card.Body>
-                                  <Card.Subtitle className="mb-2 text-muted">
-                                    Contraoferta de transportista
-                                  </Card.Subtitle>
-                                  <Card.Text>uwu</Card.Text>
-                                  <Button
-                                    className="justify-content-end"
-                                    href="#"
-                                  >
-                                    Pagar
-                                  </Button>
-                                </Card.Body>
-                              </Card>
-                            </Col>
-                          </Row>
-
-                          
-                          <Card style={{ width: "auto" }}>
-                            <Card.Header>Propuesta 1</Card.Header>
-                            <Card.Body>
-                              <Card.Subtitle className="mb-2 text-muted">
-                                Contraoferta de transportista
-                              </Card.Subtitle>
-                              <Card.Text>uwu</Card.Text>
-                              <Button
-                                variant="warning"
-                                className="pagar"
-                                href="#"
-                              >
-                                Pagar
-                              </Button>
-                            </Card.Body>
-                          </Card>
-                        </Accordion.Body>
-                      </Accordion.Item>
-                    </Accordion>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
+            <Col
+              xs={6}
+              sm={6}
+              md={{ span: 4, offset: 2 }}
+              lg={{ span: 4, offset: 2 }}
+              className="align-self-end"
+            >
+              <img
+                src={img}
+                className="img"
+                alt="Una carretilla para llevar paquetes"
+              />
             </Col>
-
-
-
-
+            <Col>
+              <svg
+                className="wave"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1440 320"
+              >
+                <path
+                  fill="#fff"
+                  // fill-opacity="1"
+                  d="M0,160L48,170.7C96,181,192,203,288,186.7C384,171,480,117,576,112C672,107,768,149,864,165.3C960,181,1056,171,1152,149.3C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+                ></path>
+              </svg>
+            </Col>
           </Row>
+        </section>
+        <br></br>
+        <Row
+          className="ml-5 justify-content-center"
+          style={{ marginLeft: "22px" }}
+        >
+          <Col xs={6} sm={6} md={3} lg={3} className="mb-5">
+            <Card
+              style={{ width: "auto", borderRadius: "20px", elevation: 3 }}
+              className="Cardsita"
+            >
+              <Card.Header
+                style={{
+                  backgroundColor: "#53b7f1",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+              >
+                Oferta 1
+              </Card.Header>
+              <Card.Body>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Descripcion breve de la oferta
+                </Card.Subtitle>
+                <Card.Text>
+                  <Accordion defaultActiveKey="1" flush>
+                    <Accordion.Item eventKey="0">
+                      <Accordion.Header>Ver propuestas</Accordion.Header>
+                      <Accordion.Body>
+                        <Card style={{ width: "auto" }}>
+                          <Card.Header>Propuesta 1</Card.Header>
+                          <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">
+                              Fecha y Hora
+                            </Card.Subtitle>
+                            <Card.Text>Precio</Card.Text>
+                            <Button
+                              variant="warning"
+                              id="pagar"
+                              href="#"
+                              onClick={handleShow}
+                            >
+                              Pagar
+                            </Button>
+                          </Card.Body>
+                        </Card>
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
 
-
-          
-        </div>
-      </React.Fragment>
-    );
-  }
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Informacion Del Conductor</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Nombre, Apellido, vehiculo placa.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              Aceptar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </React.Fragment>
+  );
 }
+
+
+export default AceptarOferta;
--- front/src/pages/Perfil.jsx
+++ front/src/pages/Perfil.jsx
@@ -6,117 +6,142 @@ import {
   Container,
   Button,
   FloatingLabel,
-  FormControl,
   Form,
+  Card,
 } from "react-bootstrap";
 
-import img from "../assets/img3.svg";
+import img from "../assets/buble.png";
 import user from "../assets/usuario.png";
 
 export default function Perfil() {
   return (
     <React.Fragment>
       <Container fluid className="p-0">
-        <Row className="justify-content-center " style={{ marginTop: "22px" }}>
-          <Col
-            xs={6}
-            sm={6}
-            md={{ span: 5, offset: 1 }}
-            lg={{ span: 5, offset: 1 }}
-            className="align-self-start"
-          >
-            <div className="conten">
-              <Row className="justify-content-center ">
-                <Col md="auto">
-                  <img src={user} className="user" />
-                </Col>
-              </Row>
-              <br></br>
-
-              <Row>
-                <Col xs={6} sm={6} md={6} lg={6}>
-                  <FloatingLabel
-                    controlId="floatingInput"
-                    label="Nombre"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="nombre" />
-                  </FloatingLabel>
-                </Col>
-                <Col xs={6} sm={6} md={6} lg={6}>
-                  <FloatingLabel
-                    controlId="floatingInput"
-                    label="Apellido"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="apellido" />
-                  </FloatingLabel>
-                </Col>
-                <Col xs={6} sm={6} md={6} lg={6}>
-                  <FloatingLabel
-                    controlId="floatingInput"
-                    label="Cedula"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="Cedula" />
-                  </FloatingLabel>
-                </Col>
-                <Col xs={6} sm={6} md={6} lg={6}>
-                  <FloatingLabel
-                    controlId="floatingInput"
-                    label="Telefono"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="telefono" />
-                  </FloatingLabel>
-                </Col>
-                <Col xs={6} sm={6} md={6} lg={6}>
-                  <FloatingLabel
-                    controlId="floatingInput"
-                    label="Correo"
-                    className="mb-3"
-                  >
-                    <Form.Control type="email" placeholder="xxxx@xxxx.com" />
-                  </FloatingLabel>
-                </Col>
-                <Col xs={6} sm={6} md={6} lg={6}>
-                  <FloatingLabel
-                    controlId="floatingInput"
-                    label="Nombre de Usuario"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="Nombre de Usuario" />
-                  </FloatingLabel>
-                </Col>
-                <Col
-                  xs={6}
-                  sm={6}
-                  md={{ span: 4, offset: 8 }}
-                  lg={{ span: 4, offset: 8 }}
-                  className="align-self-end mt-3 "
+        <Row className="justify-content-center mt-2 mb-2">
+          <Col md={10}>
+            <Card border="primary" style={{ overflow: "hidden" }}>
+              <Card.Body style={{ paddingTop: "0px", paddingBottom: "0px"}}>
+                <Row
+                  className="justify-content-center "
+                  
                 >
-                  <Button variant="outline-warning" >Editar Informacion</Button>{" "}
-                </Col>
-              </Row>
-            </div>
-          </Col>
+                  <Col
+                    xs={12}
+                    sm={12}
+                    md={12}
+                    lg={6}
+                    // md={{ span: 5, offset: 1 }}
+                    // lg={{ span: 5, offset: 1 }}
+                    className="align-self-start"
+                  >
+                    <div className="conten">
+                      <Row className="justify-content-center ">
+                        <Col md="auto">
+                          <img
+                            src={user}
+                            className="user"
+                            alt="icono de usuario"
+                          />
+                        </Col>
+                      </Row>
+                      <br></br>
 
-          <Col xs={6} sm={6} md={6} lg={6}>
-            {/* <img src={blob} className="blob" /> */}
-            <p className="texto">TIPO DE USUARIO</p>
-            <svg
-              viewBox="0 0 160 180"
-              xmlns="http://www.w3.org/2000/svg"
-              className="blob"
-            >
-              <path
-                className="bubli"
-                fill="#43CBFF"
-                d="M52.8,-56.8C64.1,-41.5,66,-20.8,67.2,1.2C68.5,23.3,69.1,46.5,57.8,61.2C46.5,75.8,23.3,81.9,3.4,78.5C-16.5,75.1,-33,62.3,-48.3,47.6C-63.6,33,-77.7,16.5,-77.9,-0.2C-78.1,-16.9,-64.4,-33.8,-49.1,-49.1C-33.8,-64.4,-16.9,-78.1,1.9,-80.1C20.8,-82,41.5,-72.2,52.8,-56.8Z"
-                transform="translate(100 80)"
-              />
-            </svg>
-            <img src={img} className="img1" />
+                      <Row>
+                        <Col xs={6} sm={6} md={6} lg={6}>
+                          <FloatingLabel
+                            controlId="floatingInput"
+                            label="Nombre"
+                            className="mb-3"
+                          >
+                            <Form.Control type="text" placeholder="nombre" />
+                          </FloatingLabel>
+                        </Col>
+                        <Col xs={6} sm={6} md={6} lg={6}>
+                          <FloatingLabel
+                            controlId="floatingInput"
+                            label="Apellido"
+                            className="mb-3"
+                          >
+                            <Form.Control type="text" placeholder="apellido" />
+                          </FloatingLabel>
+                        </Col>
+                        <Col xs={6} sm={6} md={6} lg={6}>
+                          <FloatingLabel
+                            controlId="floatingInput"
+                            label="Cedula"
+                            className="mb-3"
+                          >
+                            <Form.Control type="text" placeholder="Cedula" />
+                          </FloatingLabel>
+                        </Col>
+                        <Col xs={6} sm={6} md={6} lg={6}>
+                          <FloatingLabel
+                            controlId="floatingInput"
+                            label="Telefono"
+                            className="mb-3"
+                          >
+                            <Form.Control type="text" placeholder="telefono" />
+                          </FloatingLabel>
+                        </Col>
+                        <Col xs={6} sm={6} md={6} lg={6}>
+                          <FloatingLabel
+                            controlId="floatingInput"
+                            label="Correo"
+                            className="mb-3"
+                          >
+                            <Form.Control
+                              type="email"
+                              placeholder="xxxx@xxxx.com"
+                            />
+                          </FloatingLabel>
+                        </Col>
+                        <Col xs={6} sm={6} md={6} lg={6}>
+                          <FloatingLabel
+                            controlId="floatingInput"
+                            label="Nombre de Usuario"
+                            className="mb-3"
+                          >
+                            <Form.Control
+                              type="text"
+                              placeholder="Nombre de Usuario"
+                            />
+                          </FloatingLabel>
+                        </Col>
+                        <Col
+                          xs={6}
+                          sm={6}
+                          md={{ span: 4, offset: 8 }}
+                          lg={{ span: 4, offset: 8 }}
+                          className="align-self-end mt-3 "
+                        >
+                          <Button variant="warning">
+                            Editar Informacion
+                          </Button>{" "}
+                        </Col>
+                      </Row>
+                    </div>
+                  </Col>
+
+                  <Col
+                    xs={12}
+                    sm={12}
+                    md={12}
+                    lg={6}
+                    
+                  >
+                    {/* <img src={blob} className="blob" /> */}
+                   
+                      <p className="texto">TIPO DE USUARIO</p>
+                      <img
+                        src={img}
+                        className="img1"
+                        alt="imagen de un señor con cajas"
+                      />
+                    
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
